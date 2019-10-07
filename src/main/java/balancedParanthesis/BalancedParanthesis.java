948c7c84e22ae02697b8b23e58cccd0983f5992e
Santosh Ganti <sai.santosh.kumar.ganti@sap.com>
2019-10-07 12:55:37 -0400

    Bloomber leetcode questions

---
+++ src/main/java/balancedParanthesis/BalancedParanthesis.java
@@ -0,0 +1,43 @@
+package balancedParanthesis;
+
+import java.util.Stack;
+
+public class BalancedParanthesis {
+    private static char[][] TOKENS = {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("[])"));
+    }
+
+    private static boolean openTerm(char c) {
+        for (char[] array : TOKENS) {
+            if (array[0] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean matches(char openTerm, char closeTerm) {
+        for (char[] array : TOKENS) {
+            if (array[0] == openTerm) {
+                return array[1] == closeTerm;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : expression.toCharArray()) {
+            if (openTerm(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || !matches(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/main/java/spiralmatrix/SpiralMatraix.java
@@ -0,0 +1,13 @@
+package spiralmatrix;
+
+import java.util.List;
+
+public class SpiralMatraix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
