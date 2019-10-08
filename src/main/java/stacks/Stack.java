c9fc72a3bbfa4e624d5ac91b6fa29d4b12dd87c3
Santosh Ganti <santosh.ganti@me.com>
2019-10-07 22:28:13 -0400

    Refactoring files

--- src/main/java/stacks/Stack.java
+++ src/main/java/stacks/Stack.java
@@ -1,4 +1,32 @@
 package stacks;
 
 public class Stack {
+    private static class Node {
+        private int data;
+        private Node next;
+
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node top;
+
+    public boolean isEmpty() {
+
+        return top == null;
+    }
+
+    public int peek() {
+        return top.data;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+
+    }
+
+    public int pop(){
+        return 0;
+    }
 }
