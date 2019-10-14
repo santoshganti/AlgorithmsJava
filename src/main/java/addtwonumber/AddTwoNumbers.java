2d188b50d406a76c220c88dcd1c76a4838211f9e
Santosh Ganti <santosh.ganti@me.com>
2019-10-13 21:28:51 -0400

    LinkedLists

--- src/main/java/addtwonumber/AddTwoNumbers.java
+++ src/main/java/addtwonumber/AddTwoNumbers.java
@@ -1,46 +1,43 @@
 package addtwonumber;
 
+import java.util.List;
+
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy_head = new ListNode(0);
-        ListNode l3 = dummy_head;
-
+        ListNode dummyHead = new ListNode(0);
         int carry = 0;
 
-        while (l1 != null || l2 != null){
+        while (l1.next != null || l2.next != null) {
             int l1_val = (l1 != null) ? l1.val : 0;
             int l2_val = (l2 != null) ? l2.val : 0;
 
-            int current_sum = l1_val + l2_val + carry;
-            carry = current_sum / 10;
-            int last_digit = current_sum % 10;
+            int currentSum = l1_val + l2_val + carry;
+            carry = currentSum / 10;
+            int lastDigit = currentSum % 10;
 
-            ListNode new_node = new ListNode(last_digit);
-            l3.next = new_node;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-            l3 = l3.next;
+            ListNode newNode = new ListNode(lastDigit);
+            dummyHead.next = newNode;
         }
 
         if (carry > 0) {
-            ListNode new_node = new ListNode(carry);
-            l3.next = new_node;
-            l3 = l3.next;
+            ListNode newNode = new ListNode(carry);
+            dummyHead.next = newNode;
         }
-        return dummy_head.next;
+
+        return dummyHead.next;
     }
 
     public static class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        ListNode l1 = new ListNode(1);
     }
 
 
