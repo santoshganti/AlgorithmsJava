32c7e12ae01d2bc885c40997f6ff2da925ce8187
Santosh Ganti <santosh.ganti@me.com>
2019-11-10 17:12:52 -0500

    new node

--- src/main/java/trie/Trie.java
+++ src/main/java/trie/Trie.java
@@ -1,8 +1,5 @@
 package trie;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Trie {
     private final TrieNode root;
 
@@ -52,7 +49,92 @@ public class Trie {
     }
 
     //delete
+    // Delete word from Trie
+    public void delete(String word) {
+        if (search(word)) {
+            delete(root, word, 0);
+        }
+    }
+
+    public boolean delete(TrieNode parentNode, String word, int index) {
+        //CASE 1 - some other word's prefix is same as the prefix of this word (BCDE, BCKG)
+        //CASE 2 - We are a the last character of this word and this word is a prefix of some other word (BCDE, BCDEFG)
+        //CASE 3 - Some other word is a prefix of this word (BCDE, BC)
+        //CASE 4 - No one is dependent on this word (BCDE, BCDE)
+
+        char ch = word.charAt(index);
+        TrieNode currentNode = parentNode.children.get(ch);
+
+        boolean shouldDeleteNode;
+
+        if (currentNode.children.size() > 1) {
+            System.out.println("Entering case 1");
+            delete(currentNode, word, index + 1);
+            return false;
+        }
+
+        if (index == word.length() - 1) {
+            System.out.println("Entering case 2");
+            if (currentNode.children.size() >= 1) {
+                currentNode.endOfWord = false;
+                return false;
+            } else {
+                System.out.println("Character " + ch + " has no dependency, hence deleting it from last");
+                parentNode.children.remove(ch);
+                return true;
+            }
+        }
+
+        if (currentNode.endOfWord == true) {
+            System.out.println("Entering case 3");
+            delete(currentNode, word, index + 1);
+            return false;
+        }
+
+        System.out.println("Entering case 4");
+        shouldDeleteNode = delete(currentNode, word, index + 1);
+        if (shouldDeleteNode == true) {
+            System.out.println("Character " + ch + " has no dependency, hence deleting it");
+            parentNode.children.remove(ch);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Trie t = new Trie();
+
+        //CASE#1
+        t.insert("bcde");
+        t.insert("bckg");
+        t.delete("bcde");
+        t.search("bcde");
+        t.search("bckg");
 
 
+		/*//CASE#2
+		t.insert("bcde");
+		t.insert("bcdefg");
+		t.delete("bcde");
+		t.search("bcde");
+		t.search("bcdefg");*/
+
+
+		/*//CASE#3
+		t.insert("bcde");
+		t.insert("bc");
+		t.delete("bcde");
+		t.search("bcde");
+		t.search("bcde");
+		t.search("bc");
+		t.search("b");*/
+
+
+		/*//CASE#4
+		t.insert("bcde");
+		t.delete("bcde");
+		t.search("bcde");*/
+    }
 }
 
