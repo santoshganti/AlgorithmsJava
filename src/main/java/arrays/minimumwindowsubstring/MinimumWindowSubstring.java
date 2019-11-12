a7caec43db9c7845fde6cea21c6124608c813ba0
Santosh Ganti <santosh.ganti@me.com>
2019-11-12 02:03:42 -0500

    Update MinimumWindowSubstring.java
    Formatting changes

--- src/main/java/arrays/minimumwindowsubstring/MinimumWindowSubstring.java
+++ src/main/java/arrays/minimumwindowsubstring/MinimumWindowSubstring.java
@@ -1,39 +1,45 @@
 package arrays.minimumwindowsubstring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumWindowSubstring {
-    public String minWindow(String s, String t) {
-        if(s == null || t == null) return "";
+    public String minWindowMethod2(String s, String t) {
+        if (s == null || t == null) return "";
 
         String res = "";
 
         int[] letterCount = new int[128];
-        int left = 0, count = 0, minLen = Integer.MAX_VALUE;
 
-        for(char c: t.toCharArray()){
-            ++letterCount[c];
+        int left = 0; //begining of the left window
+        int count = 0; //how many characters that are in T
+        int minLen = Integer.MAX_VALUE;
+
+        for (char c : t.toCharArray()) {
+            letterCount[c]++;
         }
 
-        for(int right = 0; right < s.length(); ++right){
-            if(--letterCount[s.charAt(right)] >= 0) ++count;
+        for (int right = 0; right < s.length(); right++) {
+            if (--letterCount[s.charAt(right)] >= 0) count++;
 
-            while(count == t.length()){
-                if(minLen > right - left + 1){
+            while (count == t.length()) {
+                if (minLen > right - left + 1) {
                     minLen = right - left + 1;
-                    res = s.substring(left,right+1);
+                    res = s.substring(left, right + 1);
                 }
 
-                if(++letterCount[s.charAt(left)] > 0){
-                    --count;
+                if (++letterCount[s.charAt(left)] > 0) {
+                    count--;
                 }
-                ++left;
+                left++;
             }
         }
 
         return res;
     }
-
+    
     public static void main(String[] args) {
         MinimumWindowSubstring minimumWindowSubstring = new MinimumWindowSubstring();
-        System.out.println(minimumWindowSubstring.minWindow("ADOBECODEBANC","ABC"));
+        System.out.println(minimumWindowSubstring.minWindowMethod2("ADOBECODEBANC", "ABC"));
     }
 }
