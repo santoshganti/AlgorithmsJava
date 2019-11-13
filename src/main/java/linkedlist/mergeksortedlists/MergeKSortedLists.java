eab253fde4b06f6252d4484d96c96a17fecaaaf8
Santosh Ganti <santosh.ganti@me.com>
2019-11-13 03:05:45 -0500

    new leetcode problems

--- src/main/java/linkedlist/mergeksortedlists/MergeKSortedLists.java
+++ src/main/java/linkedlist/mergeksortedlists/MergeKSortedLists.java
@@ -1,4 +1,57 @@
 package linkedlist.mergeksortedlists;
 
+import java.util.PriorityQueue;
+
 public class MergeKSortedLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (ListNode head : lists) {
+            while (head != null) {
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+
+        while (!minHeap.isEmpty()) {
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        /*
+        1->4->5,
+        1->3->4,
+        2->6
+         */
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(4);
+        list1.next.next = new ListNode(5);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode list3 = new ListNode(2);
+        list3.next = new ListNode(6);
+        ListNode[] lists = {list1,list2,list3};
+        System.out.println(mergeKLists(lists));
+
+
+    }
 }
