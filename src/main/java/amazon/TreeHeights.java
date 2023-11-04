716cc83dbd37d546c436814851d634087e93dd47
Santosh Ganti <santosh.ganti@live.com>
2023-11-03 22:03:10 -0700

    updating exception handling

--- src/main/java/amazon/TreeHeights.java
+++ src/main/java/amazon/TreeHeights.java
@@ -41,12 +41,15 @@ public class TreeHeights {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
-        int[] nums = new int[N];
-        for (int i = 0; i < N; i++) {
-            nums[i] = sc.nextInt();
+        int X;
+        int[] nums;
+        try (Scanner sc = new Scanner(System.in)) {
+            int N = sc.nextInt();
+            X = sc.nextInt();
+            nums = new int[N];
+            for (int i = 0; i < N; i++) {
+                nums[i] = sc.nextInt();
+            }
         }
         System.out.println(TreeHeightsSolution1(nums, X));
     }
